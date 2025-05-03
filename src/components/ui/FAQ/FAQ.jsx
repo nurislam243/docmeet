28223caff900e6921313395ca2748c2d3dec5581
nurislam243 <mdnurislam@gmail.com>
2025-05-03 23:06:05 +0600

    added FAQ section

--- src/components/ui/FAQ/FAQ.jsx
+++ src/components/ui/FAQ/FAQ.jsx
@@ -23,19 +23,20 @@ const FAQ = () => {
             {
                 loading ? <LoadingSpinner></LoadingSpinner> :
                     <section className="dark:bg-gray-100 dark:text-gray-800">
-                        <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
-                        <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
                         <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
                                     
                             {
                                 faqData.map(singleFaq => {
-                                    <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
-                                        
-                                            <div>
-                                                <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
-                                                <p className="mt-1 dark:text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+                                        return (
+                                            <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+                                            
+                                                <div>
+                                                    <h3 className="font-semibold text-lg"><span className='font-extrabold mr-2 dark-1'>Q:</span>{singleFaq.question}</h3>
+                                                    <p className="mt-1 dark:text-gray-600"><span className='font-extrabold mr-2 dark-2'>A:</span>{singleFaq.answer}</p>
+                                                </div>
                                             </div>
-                                        </div>
+                                        )
+                                            
                                 
                                 })
                             }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,15 +18,15 @@ const Home = () => {
                 <title>Home | DocMeet</title>
             </Helmet>
             <Banner></Banner>           
-            <div className="bg-[#EFEFEF] py-[80px]">
-                <div className="text-center mb-8">
+            <div className="bg-[#EFEFEF] py-[80px] @container">
+                <div className="text-center mb-8 @min-[400px]:px-7 @min-[630px]:px-12">
                     <h1 className='font-extrabold text-[40px] text-[#0F0F0F]'>Our Best Doctors</h1>
                     <p className='leading-6 dark-1 mx-1 mt-4'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a <br /> routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
                 </div>
                 <Doctors data={data}></Doctors>
 
-                <div className="mt-20">
-                    <div className="text-center">
+                <div className="mt-20 @container">
+                    <div className="text-center @min-[400px]:px-7 @min-[630px]:px-12">
                         <h1 className='font-extrabold text-[40px] text-[#0F0F0F] mb-4'>We Provide Best Medical Services</h1>
                         <p className='text-[#0F0F0F] mb-8'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. </p>
                     </div>
@@ -39,7 +39,16 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <FAQ></FAQ>
+            {/* FAQ section */}
+            <div className="mt-4 bg-[#EFEFEF] pt-2 pb-[111px] px-2 @min-[350px]:px-3 @min-[400px]:px-4 @min-[500px]:px-5 @min-[640px]:px-12 @min-[768px]:px-6 @min-[1000px]:px-12 @min-[1550px]:px-[120px]  @min-[1750px]:px-[160px]">
+                <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
+                <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+                <div className="flex flex-col group/all p-6 space-y-6 overflow-hidden rounded-2xl bg-white dark:text-gray-800">
+                    <div className="flex space-x-4 justify-between">
+                        <FAQ></FAQ>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
